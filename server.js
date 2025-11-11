825a1a0d38ae024c01350d6fa06417db9e41c7fa
Mrinmoy <minmoydholey0@gmail.com>
2025-11-11 13:20:35 +0530

    add

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 // -------------------- Middleware --------------------
 app.use(
   cors({
-    origin: process.env.CLIENT_ORIGIN || "*",
+    origin: "https://paddy-purchase.vercel.app",
     credentials: true,
   })
 );
